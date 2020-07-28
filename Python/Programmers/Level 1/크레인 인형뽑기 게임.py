e8bdcf83e97cba08ee10e9ed08ded3b31d925259
ksb8320 <ksb8320@gmail.com>
2020-07-29 01:51:42 +0900

    commit

---
+++ Python/Programmers/Level 1/크레인 인형뽑기 게임.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin=open("input.txt")
+
+def solution(board, moves):
+    lst=[]
+    answer=0
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1]!=0:
+                if not lst:
+                    lst.append(board[j][i-1])
+                    board[j][i-1]=0
+                    break
+                else:
+                    if lst[-1]==board[j][i-1]:
+                        lst.pop()
+                        answer+=2
+                        break
+                    else:
+                        lst.append(board[j][i-1])
+                        break
+        board[j][i-1]=0
+    return answer
